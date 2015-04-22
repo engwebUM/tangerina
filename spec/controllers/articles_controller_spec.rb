76b068f3f8333c09004743c0e3d1c7e6c9dca0ed
DuarteDuarte <dd3m@hotmail.com>
2015-04-22 09:55:16 +0100

    rubocop

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -19,7 +19,6 @@ require 'rails_helper'
 # that an instance is receiving a specific message.
 
 RSpec.describe ArticlesController, type: :controller do
-
   # This should return the minimal set of attributes required to create a valid
   # Article. As you add validations to Article, be sure to
   # adjust the attributes here as well.
