5a57a1d9b0a263c42cb2aa10c6b6c57881e0aad0
Filipe Pereira <filas21pereira@gmail.com>
2015-04-22 11:36:07 +0100

    delete module SubscriptionSearch and themes_subscrived method

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -70,17 +70,6 @@ class SubscriptionsController < ApplicationController
   end
 
   
-  def themes_subscribed
-    @subscriptions = Subscription.all
-    themes = []
-
-    @subscriptions.each do |subscription|
-      if subscription.user.id == current_user.id
-        themes << subscription.theme.name
-      end
-    end  
-    themes
-  end
 
  
   private
