1939a810246cc47ff008fad57a57466816d16d79
DuarteDuarte <dd3m@hotmail.com>
2015-04-29 14:24:04 +0100

    trying to send email from heroku

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "localhost:3000",
+  :domain               => "tangerina.herokuapp.com",
   :user_name            => "tangerina.ess",
   :password             => "tangerinaess",
   :authentication       => "plain",
