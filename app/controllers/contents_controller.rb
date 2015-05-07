14ba3fba3b8754c5adfa75ea68e1a29d0d545065
guterres <guterres25@gmail.com>
2015-05-07 03:21:07 +0100

    controller files

---
+++ app/controllers/contents_controller.rb
@@ -0,0 +1,19 @@
+class ContentsController < ApplicationController
+  def new
+    @content = Content.new
+  end
+
+  def create
+    @content = Content.create(content_params)
+    if @content.save
+      render json: { message: 'success' }, status: 200
+    else
+      render json: { error @content.errors.full_messages.join(', ') }, status: 400
+    end
+  end
+
+  private
+  def content_params
+    params.require(:content).permit(:file)
+  end
+end
