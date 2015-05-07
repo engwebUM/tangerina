e81f259ab7bd9727a714ba9a2edb91653545c735
guterres <guterres25@gmail.com>
2015-05-07 02:59:58 +0100

    modified model

--- app/models/article_review.rb
+++ app/models/article_review.rb
@@ -2,14 +2,15 @@ class ArticleReview < ActiveRecord::Base
   has_many :articles
   belongs_to :theme
   belongs_to :user
+  has_many :content
 
   acts_as_taggable
   acts_as_taggable_on :tags
   validates_presence_of :tag_list
-  has_attached_file :file
+  # has_attached_file :file
   accepts_nested_attributes_for :articles
   validates_presence_of :theme_id
-  validates_attachment_content_type :file, content_type: ['application/pdf', 'text/plain']
+  # validates_attachment_content_type :file, content_type: ['application/pdf', 'text/plain']
 
   scope :creates, -> { where(event: 'create', status: 'pending') }
   scope :updates, -> { where(event: 'update', status: 'pending') }
--- app/models/content.rb
+++ app/models/content.rb
@@ -1,2 +1,6 @@
 class Content < ActiveRecord::Base
+  belongs_to :article_review
+
+  has_attached_file :file
+  validates_attachment_content_type :file, content_type: ['application/pdf', 'text/plain', /\Aimage\/.*\Z/]
 end
