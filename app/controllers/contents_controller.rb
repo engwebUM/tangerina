afbce5ad702bbe48e6f491a00cd603b9b6755923
guterres <guterres25@gmail.com>
2015-05-08 14:55:19 +0100

    fix rubocop

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -8,11 +8,12 @@ class ContentsController < ApplicationController
     if @content.save
       render json: { message: 'success' }, status: 200
     else
-      render json: { error @content.errors.full_messages.join(', ') }, status: 400
+      render json: { error: @content.errors.full_messages.join(',') }, status: 400
     end
   end
 
   private
+
   def content_params
     params.require(:content).permit(:file)
   end
--- app/models/content.rb
+++ app/models/content.rb
@@ -2,5 +2,5 @@ class Content < ActiveRecord::Base
   belongs_to :article_review
 
   has_attached_file :file
-  validates_attachment_content_type :file, content_type: ['application/pdf', 'text/plain', /\Aimage\/.*\Z/]
+  validates_attachment_content_type :file, content_type: ['application/pdf', 'text/plain', 'image/jpg']
 end
