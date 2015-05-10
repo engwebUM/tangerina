cc8a9e1a604a279730d2c6d4bcb3522f16ef2e8c
guterres <guterres25@gmail.com>
2015-05-10 16:30:01 +0100

    fix imagemagick

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,10 +1,9 @@
 class Content < ActiveRecord::Base
   belongs_to :article_review
 
-  has_attached_file :file, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :file
 
   validates_attachment_content_type :file,
-          content_type: ['application/pdf', 'text/plain', 'image/jpeg'],
-          presence: true,
-          size: { less_than: 1.megabyte }
+          content_type: ['application/pdf', 'image/jpeg']
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,5 +43,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   #paperclip
-  Paperclip.options[:command_path] = 'C:/Program Files (x86)/ImageMagick-6.9.0-Q16'
+  Paperclip.options[:command_path] = 'C:\ImageMagick'
+  Paperclip.options[:swallow_stderr] = false
 end
