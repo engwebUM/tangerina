ce6a90727cba20db88a152785783c402d4e1d604
guterres <guterres25@gmail.com>
2015-05-11 22:31:43 +0100

    validate urls youtube

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,5 @@
 class Video < ActiveRecord::Base
   belongs_to :article_review
+
+  validates_format_of :link, with: /((https?:\/\/(.*youtube\.com\/watch.*|.*\.youtube\.com\/v\/.*|youtu\.be\/.*|.*\.youtube\.com\/user\/.*|.*\.youtube\.com\/.*#.*\/.*|m\.youtube\.com\/watch.*|m\.youtube\.com\/index.*|.*\.youtube\.com\/profile.*|.*\.youtube\.com\/view_play_list.*|.*\.youtube\.com\/playlist.*|www\.youtube\.com\/embed\/.*|youtube\.com\/gif.*|www\.youtube\.com\/gif.*))|(https:\/\/(.*youtube\.com\/watch.*|.*\.youtube\.com\/v\/.*|.*\.youtube\.com\/playlist.*|www\.youtube\.com\/embed\/.*|youtube\.com\/gif.*|www\.youtube\.com\/gif.*)))/i
 end
