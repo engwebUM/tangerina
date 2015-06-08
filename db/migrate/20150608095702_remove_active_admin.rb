1413f19f8fdb0c8f1b9b62f16df361f689540401
guterres <guterres25@gmail.com>
2015-06-08 11:05:28 +0100

    drop table active_admin

---
+++ db/migrate/20150608095702_remove_active_admin.rb
@@ -0,0 +1,5 @@
+class RemoveActiveAdmin < ActiveRecord::Migration
+  def change
+    drop_table :active_admin_comments
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150602185436) do
-
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string   "namespace"
-    t.text     "body"
-    t.string   "resource_id",   null: false
-    t.string   "resource_type", null: false
-    t.integer  "author_id"
-    t.string   "author_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
-  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+ActiveRecord::Schema.define(version: 20150608095702) do
 
   create_table "article_reviews", force: :cascade do |t|
     t.integer  "article_id"
