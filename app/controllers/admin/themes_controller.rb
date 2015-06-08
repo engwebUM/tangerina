667853e2283d1393dd047519a6461b91bf12cd2f
guterres <guterres25@gmail.com>
2015-06-08 14:27:56 +0100

    added controller and view

---
+++ app/controllers/admin/themes_controller.rb
@@ -0,0 +1,40 @@
+module Admin
+  class ThemesController < AdminController
+    before_filter :authorize
+
+    def index
+      @themes = Theme.all
+      @theme = Theme.new
+    end
+
+    def create
+      @theme = Theme.new(theme_params)
+      if @theme.save
+        # redirect_to action: :index
+        index
+      else
+        render :new
+      end
+    end
+
+    def destroy
+      @theme.destroy
+      redirect_to themes_url
+      # respond_to do |format|
+      # format.html { redirect_to themes_url }
+      # format.json { head :no_content }
+      # end
+    end
+
+    private
+
+    def set_theme
+      @theme = Theme.find(params[:id])
+    end
+
+    def theme_params
+      params.require(:theme).permit(:name)
+    end
+
+  end
+end
