033d82846e7f42f026a1ce06dc9f7df7429ec5fa
guterres <guterres25@gmail.com>
2015-06-15 19:24:36 +0100

    add file image to theme

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608095702) do
+ActiveRecord::Schema.define(version: 20150615171056) do
 
   create_table "article_reviews", force: :cascade do |t|
     t.integer  "article_id"
@@ -93,8 +93,12 @@ ActiveRecord::Schema.define(version: 20150608095702) do
 
   create_table "themes", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
