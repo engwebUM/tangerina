f98937d79bc0c9fc2a9e376c0457882238db8e2c
guterres <guterres25@gmail.com>
2015-06-15 20:30:47 +0100

    fix rubocop

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,11 +1,11 @@
 class Theme < ActiveRecord::Base
-  has_attached_file :image, styles: { small: "150x150>" },
-                  url: '/assets/themes/:id/:style/:basename.:extension',
-                  path: ':rails_root/public/assets/themes/:id/:style/:basename.:extension'
+  att_url = '/assets/themes/:id/:style/:basename.:extension'
+  att_path = ':rails_root/public/assets/themes/:id/:style/:basename.:extension'
+
+  has_attached_file :image, styles: { small: '150x150>' }, url: att_url, path: att_path
 
   validates_attachment_presence :image
   validates_attachment_content_type :image, content_type: ['image/png', 'image/jpeg']
-
 
   has_many :article_reviews
   has_many :subscriptions
