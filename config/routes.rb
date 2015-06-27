0209bb7b8422562594c604ecf638603d6782c02a
guterres <guterres25@gmail.com>
2015-06-27 04:46:45 +0100

    move to admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get '', to: 'dashboard#index', as: '/'
+    resources :revisers
   end
 
   resources :favorites
-  resources :revisers
   resources :subscriptions
   resources :contents
   resources :articles do
