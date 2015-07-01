00493ddf528d4f6cc5e2735d8d6cab2ac7ea0de7
guterres <guterres25@gmail.com>
2015-07-02 00:06:04 +0100

    fix js

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,3 +1,37 @@
+//= require jquery-ui
 //= require fileinput.min
 //= require fancybox
 //= require fancybox-media
+
+
+  $( "#datepicker" ).datepicker({
+    dateFormat: "dd-mm-yy"
+  });
+
+  $( "#datepicker2" ).datepicker({
+    dateFormat: "dd-mm-yy"
+  });
+
+  var max_fields = 5;
+  var wrapper = $("#input_fields_wrap");
+  var add_button = $("#add_field_button");
+
+  var x = 1;
+  $(add_button).click(function(e){
+    e.preventDefault();
+    if(x < max_fields){
+      x++;
+      $(wrapper).append('<div class="form-group"><input type="text" class="form-control" name="videos[link][]"/><a href="#" id="remove_field">Remove</a></div>');
+    }
+  });
+
+  $(wrapper).on("click","#remove_field", function(e){ //user click on remove text
+      e.preventDefault(); $(this).parent('div').remove(); x--;
+  })
+
+  $('#file-1').fileinput({
+    showUpload: false,
+    showCaption: false,
+    allowedFileExtensions: ['jpg', 'png', 'pdf'],
+
+  });
--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,2 +1,18 @@
 //= require fancybox
 //= require fancybox-media
+
+
+
+(function()
+{
+  if( window.localStorage )
+  {
+    if( !localStorage.getItem('firstLoad') )
+    {
+      localStorage['firstLoad'] = true;
+      window.location.reload();
+    }
+    else
+      localStorage.removeItem('firstLoad');
+  }
+})();
