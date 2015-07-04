7158b68bf9a6b14d4566a975f39c3905665b3c6d
guterres <guterres25@gmail.com>
2015-07-04 05:09:22 +0100

    improvement controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,67 +1,61 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  # before_action :set_article_comment, only: [:show, :edit, :update, :destroy]
 
   def index
-    @comments = Comment.all
-  end
-
-  def show
-  end
-
-  def new
-  end
-
-  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    # respond_to do |format|
+    @comment.user_id = current_user.id if current_user
     if @comment.save
-      redirect_to :back
-      # respond_with :show, location: @comment
-      # format.html { redirect_to :back, notice: 'Comment was successfully created.' }
-      # format.json { render :show, status: :created, location: @comment }
-      # format.js { render inline: 'location.reload();' }
+      redirect_to article_path(@article)
     else
-      # format.html { render :new }
-      # format.json { render json: @comment.errors, status: :unprocessable_entity }
-      redirect_to :back
+      render 'new'
     end
-    # end
+  end
+
+  def edit
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
   end
 
   def update
-    # respond_to do |format|
-    if @comment.update(comment_params)
-      redirect_to @comment
-      # format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-      # format.json { render :show, status: :ok, location: @comment }
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    if @comment.user_id == current_user.id
+      respond_to do |format|
+        if @comment.update(comment_params)
+          format.html { redirect_to @article }
+          format.json { render json: @article }
+        else
+          format.html { render 'edit' }
+          format.json { render json: @article }
+        end
+      end
     else
-      render :edit
-      # format.html { render :edit }
-      # format.json { render json: @comment.errors, status: :unprocessable_entity }
+      redirect_to article_path(@article)
     end
-    # end
   end
 
   def destroy
-    @comment.destroy
-    redirect_to comments_url
-    # respond_to do |format|
-    #  format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-    #  format.json { head :no_content }
-    # end
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    if @comment.user_id == current_user.id
+      @comment.destroy
+      redirect_to article_path(@article)
+    end
   end
 
   private
 
-  def set_comment
+  def set_article_comment
+    @article = Article.find(params[:article_id])
     @comment = Comment.find(params[:id])
   end
 
   def comment_params
-    params.require(:comment).permit(:article_id, :user_id, :body)
+    params.require(:comment).permit(:body)
   end
 end
