ca00f3368ae2ef9e88a5eda8279b8b3da250c19e
guterres <guterres25@gmail.com>
2015-07-05 21:04:54 +0100

    layout tables

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,6 +1,8 @@
 //= require fancybox
 //= require fancybox-media
+//= require jquery-ui
 //= require dataTables/jquery.dataTables
 $("table#versions").dataTable({
-  bJQueryUI: true
+  bJQueryUI: true,
+  "oLanguage": { "sSearch": "" }
 });
