fbae47fbecf7a12ef84b9c4175ba8dea2c8d3439
guterres <guterres25@gmail.com>
2015-07-05 01:15:01 +0100

    style article finish

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -11,6 +11,7 @@ class Theme < ActiveRecord::Base
   att_path = ':rails_root/public/assets/themes/:id/:style/:basename.:extension'
 
   has_attached_file :image, styles: { small: '150x150>' }, url: att_url, path: att_path
+  has_attached_file :image, styles: { medium: '340x215>' }, url: att_url, path: att_path
 
   validates_attachment_presence :image
   validates_attachment_content_type :image, content_type: ['image/png', 'image/jpeg']
