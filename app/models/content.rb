f15a1759de4284aacc5cd8dab308eb67fd4495b2
guterres <guterres25@gmail.com>
2015-07-08 04:29:57 +0100

    fix some bugs

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,6 +2,6 @@ class Content < ActiveRecord::Base
   belongs_to :article_review
 
   has_attached_file :file, styles: { normal: "500x500>"}
-  validates_attachment_content_type :file, less_than: 3.megabytes
+  validates_attachment_size :file, less_than: 3.megabytes
   validates_attachment_content_type :file, content_type: ['image/jpeg', 'image/png']
 end
