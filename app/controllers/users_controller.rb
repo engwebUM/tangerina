d1c3e9357232f03e0058b9adb47192aa8d578fd6
guterres <guterres25@gmail.com>
2015-07-08 08:16:25 +0100

    new fields for table users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,23 +13,44 @@ class UsersController < Clearance::UsersController
 
   private
 
+  # rubocop:disable all
   def user_from_params
     email = user_params.delete(:email)
     password = user_params.delete(:password)
     username = user_params.delete(:username)
+    user_info(email, password, username)
+  end
+  # rubocop:disable all
 
-    clearance_configuration(email, password, username)
+  def user_info(email, password, username)
+    first_name = user_params.delete(:first_name)
+    last_name = ser_params.delete(:last_name)
+    city = ser_params.delete(:city)
+    country = ser_params.delete(:country)
+    occupation = ser_params.delete(:occupation)
+    about = ser_params.delete(:about)
+    date_birth = ser_params.delete(:date_birth)
+    photo = ser_params.delete(:photo)
+    clearance_configuration(email, password, username, first_name, last_name, city, country, occupation, about, date_birth, photo)
   end
 
   def set_user
     @user = User.find(params[:id])
   end
 
-  def clearance_configuration(email, password, username)
+  def clearance_configuration(email, password, username, first_name, last_name, city, country, occupation, about, date_birth, photo)
     Clearance.configuration.user_model.new(user_params).tap do |user|
       user.email = email
       user.password = password
       user.username = username
+      user.first_name = first_name
+      user.last_name = last_name
+      user.city = city
+      user.country = country
+      user.occupation = occupation
+      user.about = about
+      user.date_birth = date_birth
+      user.photo = photo
     end
   end
 end
