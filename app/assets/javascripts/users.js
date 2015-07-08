e00ea3057732d08a856f788bcfd641d98bf88a18
guterres <guterres25@gmail.com>
2015-07-08 15:48:13 +0100

    add favorites in profile

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,10 @@
 //= require jquery-ui/datepicker
+//= require dataTables/jquery.dataTables
+
 $( "#datepicker" ).datepicker({
   dateFormat: "dd-mm-yy"
+});
+$("table#versions").dataTable({
+  bJQueryUI: true,
+  "oLanguage": { "sSearch": "" }
 });
