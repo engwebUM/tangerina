d897c4a94a5e002a0c33f09df6d99e400fd309cd
guterres <guterres25@gmail.com>
2015-07-08 23:53:07 +0100

    add profile in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
   resources :passwords, controller: 'clearance/passwords', only: [:create, :new]
   resource :session, controller: 'clearance/sessions', only: [:create]
 
-  resources :users, controller: 'users', only: [:create, :index, :show] do
+  resources :users, controller: 'users', only: [:create, :show] do
     resource :password, controller: 'clearance/passwords', only: [:index, :show, :create, :edit, :update]
 
   end
